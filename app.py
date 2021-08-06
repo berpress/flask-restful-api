aea0a78e62305564df21b1f9b123f934e58d84e5
Alexander Lozovoy <berpress@gmail.com>
2021-08-06 23:17:16 +0300

    Fix start app in heroku

--- app.py
+++ app.py
@@ -17,12 +17,6 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "jose"
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, "/store/<string:name>")
@@ -39,4 +33,11 @@ if __name__ == "__main__":
     from db import db
 
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config["DEBUG"]:
+
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
