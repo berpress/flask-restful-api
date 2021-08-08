6e12e5d00b46895a8af8da9b0bdb499853492bcf
Alexander Lozovoy <berpress@gmail.com>
2021-08-08 16:03:51 +0300

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,45 @@
 import pytest
 
 # First party modules
-from app2 import create_app
+# from app2 import create_app
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from resources.balance import Balance
+from resources.item import Item, ItemList
+from resources.pay import Pay
+from resources.store import Store, StoreList
+from resources.user import UserRegister
+from resources.user_info import UserInfo
+from security import authenticate, identity
+from flask_restful import Api
+from flask_jwt import JWT
+
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.secret_key = "jose"
+    api = Api(app)
+
+    JWT(app, authenticate, identity)  # /auth
+
+    api.add_resource(Store, "/store/<string:name>")
+    api.add_resource(StoreList, "/stores")
+    api.add_resource(Item, "/item/<string:name>")
+    api.add_resource(ItemList, "/items")
+    api.add_resource(UserRegister, "/register")
+    api.add_resource(Balance, "/balance/<int:uuid>")
+    api.add_resource(Pay, "/pay/<int:uuid>")
+    api.add_resource(UserInfo, "/user_info/<int:uuid>")
+    return app
 
 
 @pytest.fixture
--- tests/functional/test_client.py
+++ tests/functional/test_client.py
@@ -1,4 +1,31 @@
-def test_square(client):
-    payload_register = {"username": "test111", "password": "Password111"}
-    rv = client.post("/register", json=payload_register)
-    assert rv.status_code == 201
+USERNAME = "test111"
+PASSWORD = "Password111"
+
+payload = {"username": USERNAME, "password": PASSWORD}
+
+
+def test_register_user(client):
+    payload_register = payload
+    response = client.post("/register", json=payload_register)
+    assert response.status_code == 201
+    assert response.json.get("message") == "User created successfully."
+    assert response.json.get("uuid") is not None
+
+
+def test_register_user_twice(client):
+    payload_register = payload
+    response = client.post("/register", json=payload_register)
+    assert response.status_code == 400
+
+
+def test_auth_user(client):
+    payload_register = payload
+    response = client.post("/auth", json=payload_register)
+    assert response.status_code == 200
+    assert response.json.get("access_token") is not None
+
+
+def test_auth_user_invalid_data(client):
+    payload_register = {"username": "test2", "password": "PASSWORD"}
+    response = client.post("/auth", json=payload_register)
+    assert response.status_code == 401
